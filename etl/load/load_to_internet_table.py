e26c4499168f19d32ebf972745f74727aac27e0e
Demaxsuel Batista <92854524+demaxsuelmb@users.noreply.github.com>
2023-06-09 21:07:51 -0300

    load_etl

---
+++ etl/load/load_to_internet_table.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import psycopg2
+
+path_file = "data/processed/internet_processed_2.csv"
+
+
+# Convert JSON data to a DataFrame
+df = pd.read_csv(path_file)
+
+# Print the DataFrame
+print(df.columns)
+
+df = df[[
+          'index'
+        , 'entity'
+        , 'code'
+        , 'year'
+        , 'cellulars'
+        , 'percent_users'
+        , 'users'
+        , 'broadband'
+        ]]
+
+
+# # rename columns
+df = df.rename(columns={'column1': 'rename1'})
+
+# Change type of columns
+data_types={
+          'index' : int
+        , 'entity' : str
+        , 'code' : str
+        , 'year' : int
+        , 'cellulars': int
+        , 'percent_users' : float
+        , 'users' : int
+        , 'broadband' : int 
+            }
+
+# change the type
+df = df.astype(data_types)
+
+
+# PostgreSQL connection details
+conn = psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password="12345678",
+    host="localhost",
+    port="5432"
+)
+
+# schema name
+schema_name= "public"
+# table to insert the data into
+table_name = "f_internet"
+
+
+
+# # Iterate over DataFrame rows and insert into the database
+# with conn.cursor() as cursor:
+#     for index, row in df.iterrows():
+#         values = [row['lead_id'], row['lead_nome'], row['dt_cadastro'], row['updateDate'], row['mktLink'], row['publicLink'], row['stage'], row['city'], row['state'], row['country'], row['industry_id'], row['industry'], row['source_id'], row['source_name'], row['subSource_id'], row['subsource']]
+#         insert_query = f"INSERT INTO {table_name} (lead_id, lead_nome, dt_cadastro, updateDate, mktLink, publicLink, stage, city, state, country, industry_id, industry, source_id, source_name, subSource_id, subsource) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+#         cursor.execute(insert_query, values)
+
+#     conn.commit()
+
+# # Close the database connection
+# conn.close()
---
+++ etl/load/miguel.py
@@ -0,0 +1,40 @@
+
+try:
+    # Establish a connection to the PostgreSQL database
+    conn = psycopg2.connect(
+        host="your_host",
+        database="your_database",
+        user="your_user",
+        password="your_password"
+    )
+
+    # Create a cursor object to execute SQL queries
+    cur = conn.cursor()
+
+    # SQL query to create the table
+    create_table_query = '''
+        CREATE TABLE IF NOT EXISTS employees (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100),
+            age INTEGER,
+            salary NUMERIC(10, 2)
+        )
+    '''
+
+    # Execute the SQL query to create the table
+    cur.execute(create_table_query)
+
+    # Commit the transaction
+    conn.commit()
+
+    print("Table created successfully!")
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL or creating the table:", error)
+
+finally:
+    # Close the cursor and connection
+    if cur:
+        cur.close()
+    if conn:
+        conn.close()
