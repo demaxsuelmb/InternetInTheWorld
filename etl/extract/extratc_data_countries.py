306f720780e1dec1a75c9756ba67f942e993f35e
Demaxsuel Batista <92854524+demaxsuelmb@users.noreply.github.com>
2023-12-24 10:44:04 -0300

    new_extract

--- etl/extract/extratc_data_countries.py
+++ etl/extract/extratc_data_countries.py
@@ -1,19 +1,35 @@
+# Extract Data of Countries
+
+# import libraries
 import pandas as pd
+import matplotlib as plt 
 
-# Connect with second csv file
-df = pd.read_csv('C:/Users/demaxsuel.batista/Documents/Python Scripts/03 - analises e port/01 - project DE - Internet/data/raw/countries.csv', delimiter=';')
-
-# Specify the new folder path
-output_folder = 'C:/Users/demaxsuel.batista/Documents/Python Scripts/03 - analises e port/01 - project DE - Internet/data/processed/'
-
+# Connect with csv file
+df = pd.read_csv('data/raw/countries.csv', delimiter=';')
 
 # transform in a DataFrame
 df = pd.DataFrame(df)
-# print a sample of data
-print('second file')
-print(df.head())
 
+# changing comma to dot
+df = df.replace({',': '.'}, regex=True)
+
+# Basic informations
+# headers - the first few rows of the DataFrame
+print("\nhead")
+print(df.head(5))
+# info - data types and missing values
+print("\ninfo")
+print(df.info())
+# describe - statistics for numerical columns
+print("\ndescribe")
+print(df.describe())
+# Count of unique values in each column
+print("\nnunique")
+print(df.nunique())
+
+# Specify the new folder path
+output_folder = 'data/processed/'
 # Save the DataFrame as a new CSV file in the output folder
 df.to_csv(output_folder + 'countries_processed.csv', index=False)
 
-print("File saved successfully!")
+print("\nFile saved successfully!")
