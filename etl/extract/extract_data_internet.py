f5e12acee8f5c2fb29cfca79e3c38d43b703233b
Demaxsuel Batista <92854524+demaxsuelmb@users.noreply.github.com>
2023-12-26 17:27:54 -0300

    new_files

--- etl/extract/extract_data_internet.py
+++ etl/extract/extract_data_internet.py
@@ -1,17 +1,48 @@
 import pandas as pd
 
 # Connect with csv file
-df = pd.read_csv('C:/Users/demaxsuel.batista/Documents/Python Scripts/03 - analises e port/01 - project DE - Internet/data/raw/internet.csv', delimiter=';')
-
-# Specify the new folder path
-output_folder = 'C:/Users/demaxsuel.batista/Documents/Python Scripts/03 - analises e port/01 - project DE - Internet/data/processed/'
+df = pd.read_csv('data/raw/internet.csv', delimiter=';')
 
 # transform in a DataFrame
 df = pd.DataFrame(df)
-# print a sample of data
-print(df.head())
 
+
+# Convert selected columns to float using a loop
+columns_to_convert = [
+                    'Cellular Subscription'
+                    ,'Internet Users(%)'
+                    ,'No. of Internet Users'
+                    ,'Broadband Subscription'
+                    ]
+
+
+# Convert selected columns to int using a loop
+for col in columns_to_convert:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
+
+# Basic informations
+# headers - the first few rows of the DataFrame
+print("\nhead")
+print(df.head(5))
+
+# info - data types and missing values
+print("\ninfo")
+print(df.info())
+
+# describe - statistics for numerical columns
+print("\ndescribe")
+print(df.describe())
+
+# Count of unique values in each column
+print("\nnunique")
+print(df.nunique())
+
+
+# Save in the csv file
+# Specify the new folder path
+output_folder = 'data/processed/'
 # Save the DataFrame as a new CSV file in the output folder
 df.to_csv(output_folder + 'internet_processed.csv', index=False)
 
-print("File saved successfully!")
+print("\nFile saved successfully!")
--- etl/extract/extratc_data_countries.py
+++ etl/extract/extratc_data_countries.py
@@ -2,7 +2,6 @@
 
 # import libraries
 import pandas as pd
-import matplotlib as plt 
 
 # Connect with csv file
 df = pd.read_csv('data/raw/countries.csv', delimiter=';')
@@ -13,23 +12,74 @@ df = pd.DataFrame(df)
 # changing comma to dot
 df = df.replace({',': '.'}, regex=True)
 
+# List of columns to convert
+columns_to_convert = [
+                'Agriculture'
+                , 'Industry'
+                , 'Service'
+                , 'Coastline (coast/area ratio)'
+                , 'Net migration'
+                , 'Infant mortality (per 1000 births)'
+                , 'Literacy (%)'
+                , 'Phones (per 1000)'
+                , 'Arable (%)'
+                , 'Crops (%)'
+                , 'Other (%)'
+                , 'Birthrate'
+                , 'Deathrate'
+                , 'Pop. Density (per sq. mi.)'
+ ]
+
+
+# Convert selected columns to float using a loop
+for col in columns_to_convert:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
+
+df = df[[
+        'Country'
+        , 'Region'
+        , 'Climate'
+        , 'Population'
+        , 'Area (sq. mi.)'
+        , 'Pop. Density (per sq. mi.)'
+        , 'Coastline (coast/area ratio)'
+        , 'Net migration'
+        , 'Infant mortality (per 1000 births)'
+        , 'GDP ($ per capita)'
+        , 'Literacy (%)'
+        , 'Phones (per 1000)'
+        , 'Arable (%)'
+        , 'Crops (%)'
+        , 'Other (%)'
+        , 'Birthrate'
+        , 'Deathrate'
+        , 'Agriculture'
+        , 'Industry'
+        , 'Service']]
+
+
 # Basic informations
 # headers - the first few rows of the DataFrame
 print("\nhead")
 print(df.head(5))
+
 # info - data types and missing values
 print("\ninfo")
 print(df.info())
+
 # describe - statistics for numerical columns
 print("\ndescribe")
 print(df.describe())
+
 # Count of unique values in each column
 print("\nnunique")
 print(df.nunique())
 
-# Specify the new folder path
-output_folder = 'data/processed/'
-# Save the DataFrame as a new CSV file in the output folder
-df.to_csv(output_folder + 'countries_processed.csv', index=False)
+
+# # Specify the new folder path
+# output_folder = 'data/processed/'
+# # Save the DataFrame as a new CSV file in the output folder
+# df.to_csv(output_folder + 'countries_processed.csv', index=False)
 
 print("\nFile saved successfully!")
